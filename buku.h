b2429a134909359d03539a6c8022d5acdf6c1daf
Reinasoan <reina.soan123@gmail.com>
2025-11-27 19:30:34 +0700

    Update buku.h

--- buku.h
+++ buku.h
@@ -2,6 +2,19 @@
 #define BUKU_H_INCLUDED
 #include <iostream>;
 using namespace std;
+typedef struct Buku *infotype;
+typedef struct elmslist *adrbuku;
+struct Buku{
+  string idBuku;
+  string judulBuku;
+  int tahun;
+};
+struct elmlist{
+  adrbuku first;
+  adrbuku last;
+};
+
+
 
 
 #endif // BUKU_H_INCLUDED
