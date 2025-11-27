4239f703702e4adddb1e096ac91f2705b4d6b0dc
Reinasoan <reina.soan123@gmail.com>
2025-11-27 19:37:23 +0700

    Update penulis.h

--- penulis.h
+++ penulis.h
@@ -3,5 +3,16 @@
 #include "buku.h";
 #include <iostream>;
 using namespace std;
+typedef struct penulis *infotype;
+typedef struct elmlist *adrpenulis;
+struct penulis{
+  string idpenulis;
+  string namapenulis;
+  int bukuterbit;
+};
+struct elmlist{
+  adrpenulis first;
+};
+
 
 #endif // PENULIS_H_INCLUDED
