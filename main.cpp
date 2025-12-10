5561cd4fd31dd87565cc72f86cdc2a5a1c9d8bc9
Reinasoan <reina.soan123@gmail.com>
2025-12-10 16:18:51 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-
+#include "Mll.h"
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int pilih;
+
+    do {
+        cout << "\n=========== MAIN MENU ===========\n";
+        cout << "1. Menu Admin\n";
+        cout << "2. Menu User\n";
+        cout << "0. Keluar Program\n";
+        cout << "Pilih: ";
+        cin >> pilih;
+
+        if (pilih == 1) {
+            mainAdmin(L);
+        }
+        else if (pilih == 2) {
+            mainUser(L);
+        }
+        else if (pilih == 0) {
+            cout << "Program selesai.\n";
+        }
+
+    } while (pilih != 0);
+
     return 0;
+}
+
 }
